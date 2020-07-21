279fe11a226d7d64976796ced2fcff399858a65a
Brian Hague <alphaseinor@gmail.com>
2020-07-21 12:18:42 -0500

    reset

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import React from 'react';
-import { Container, Row, Col } from 'reactstrap';
+import './reset.scss'
+import "./App.scss"
 
 import { NavList } from './components/nav/NavList';
 
 function App() {
   return (
-    <Container>
-      <Row>
-        <Col>
-          <NavList />
-        </Col>
-      </Row>
-      <Row>
-        <Col xs="auto">One day, you'll see content here... for now I don't have enough time to show off my work, because of work</Col>
-      </Row>
-    </Container>
+    <>
+    <header>
+      <NavList />
+    </header>
+    <main>
+      <p>One day, you'll see content here... for now I don't have enough time to show off my work, because of work</p>
+    </main>
+    <footer>
+
+    </footer>
+    </>
   );
 }
 
