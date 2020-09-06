2de867a973817632c8bec4284c36eb081d66f4f7
Brian Hague <alphaseinor@gmail.com>
2020-09-06 01:41:07 -0500

    fixed a few issues with css

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -3,6 +3,7 @@ import packageJson from '../../../package.json'
 import Keys from './Keys'
 import markdownFile from '../../articles/tests/markdown.md'
 import Markdown from 'markdown-to-jsx'
+import cogoToast from 'cogo-toast'
 
 
 const Main = () => {
@@ -13,22 +14,32 @@ const Main = () => {
             .then(data=>data.text())
             .then(text=> {
                 setRenderMD(text)
+                cogoToast.loading(`loaded ${markdownFile}`,{position: 'bottom-center'})
             })
     } , [] )
 
 
     return (
         <main>
+            <h1>About Me:</h1>
             <section>
-                <h2>Front End Tech Stack:</h2>
-                {packageJson.dependencies && <Keys keys={packageJson.dependencies} name="Dependencies:" />}
-                {packageJson.devDependencies && <Keys keys={packageJson.devDependencies} name="Dev Dependencies:" />}
+                <article>
+                    <h2>Brian Hague</h2>
+                    <p>I like to do a lot of experimentation with different technologies using this page, so it's not usually the same from month to month</p>
+                    <p>the current stack (below) is auto generated from the package.json file, so it's always up to date</p>
+                </article>
+                <article>
+                    <h2>Front End Tech Stack:</h2>
+                    {packageJson.dependencies && <Keys keys={packageJson.dependencies} name="Dependencies:" />}
+                    {packageJson.devDependencies && <Keys keys={packageJson.devDependencies} name="Dev Dependencies:" />}
+                </article>
             </section>
             <section className="markdown">
                 <h2>
                     Experiment with markdown:
                 </h2>
-                <p>rendering {markdownFile}</p>
+                <p>This is rendered from <a href={markdownFile} target="_blank" rel="noopener noreferrer">a staic resource</a>, I ended up having to use fetch because I was trying a few different options on rendering. I found the module markdown-to-jsx works wonderfully with fetch and react hooks</p>
+                <p>I may still convert this to a module for markdown-it for some of the extra plugins that are not currently accounted for</p>
                 <Markdown children={renderMD} />
             </section>
         </main>
