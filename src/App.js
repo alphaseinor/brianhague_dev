c5ec0c5962e55250b7e76b72422231805a7904cb
Brian Hague <alphaseinor@gmail.com>
2020-09-08 03:51:36 -0500

    added state for menu object and refactored menu still needs styling and other changes

--- src/App.js
+++ src/App.js
@@ -4,16 +4,24 @@ import "./App.scss"
 
 import Header from './components/header/Header'
 import Main from './components/main/Main';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Markdown from './components/markdown/Markdown'
 
 function App() {
   return (
-    <>
-    <Header />
-    <Main />
-    <footer>
+    <Router>
+      <Header />
+      
+      <Route exact path="/">
+        <Main />
+      </Route>
+      <Route path="/markdown">
+        <Markdown />
+      </Route>
+      <footer>
 
-    </footer>
-    </>
+      </footer>
+    </Router>
   );
 }
 
---
+++ src/components/nav/NavItems.js
@@ -0,0 +1,55 @@
+import React, {useState} from 'react'
+import {Link} from 'react-router-dom'
+
+const NavItems = ({menuItem}) => {
+    const [menuToggle, setMenuToggle] = useState(false)
+    console.log(menuItem)
+    switch(menuItem.type){
+        case "toggle":
+            const toggleMenu = (e) => {
+                e.preventDefault()
+                setMenuToggle(!menuToggle)
+            }
+            return(
+                <div>
+                    <button 
+                        className = "menuToggle"
+                        onClick = {toggleMenu}
+                    >
+                        <div>
+                            <img src={menuItem.img.src} className={menuItem.img.className} alt={menuItem.img.alt} />
+                            {menuItem.text}
+                        </div>
+                    </button>
+                    <div className={menuToggle ? "links" : "hidden"}>
+                        {menuItem.subMenu.map((menuItem, index)=><NavItems key={`NavItems-menu-${index}`} menuItem={menuItem} />)}
+                    </div>
+                </div>
+            )
+        case "a":
+            return(
+                <a href={menuItem.href}>
+                    <img src={menuItem.img.src} className={menuItem.img.className} alt={menuItem.img.alt} />
+                    <p>{menuItem.text}</p>
+                </a>
+            )
+            
+        case "link":
+            return(
+                <Link to={menuItem.to}>
+                    <img src={menuItem.img.src} className={menuItem.img.className} alt={menuItem.img.alt} />
+                    <p>{menuItem.text}</p>
+                </Link>
+            )
+        default:
+            return (
+                <></>
+            )
+    }
+}
+    
+    // <a href="https://www.linkedin.com/in/brian-hague-7441908/">
+    //   <img className="svg" src="/images/linkedin-brands.svg" alt="linked in logo" />
+    //   <p>LinkedIn</p>
+    // </a>
+export default NavItems
